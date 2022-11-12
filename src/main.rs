afeaef6bc3357d9185333e909d3012e0cc71a43e
Saverio Miroddi <saverio.pub2@gmail.com>
2022-11-12 22:09:59 +0100

    Merge `c2rust-lib` module into `main`

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,56 @@
     unused_assignments,
     unused_mut
 )]
-#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::all)]
+#![allow(dead_code)]
+#![allow(mutable_transmutes)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(unused_assignments)]
+#![allow(unused_mut)]
+#![feature(c_variadic)]
+#![feature(extern_types)]
 
-// Rust port: Watch out! Clippy removes this, which causes the build to fail.
-#[allow(unused_imports)]
-use ::c2rust_out::*;
+pub mod armor;
+pub mod chase;
+pub mod command;
+pub mod curses;
+pub mod daemon;
+pub mod daemons;
+pub mod env;
+pub mod r#extern;
+pub mod fakedos;
+pub mod fight;
+pub mod init;
+pub mod io;
+pub mod list;
+pub mod load;
+pub mod mach_dep;
+pub mod maze;
+pub mod misc;
+pub mod monsters;
+pub mod r#move;
+pub mod new_leve;
+pub mod pack;
+pub mod passages;
+pub mod potions;
+pub mod protect;
+pub mod rings;
+pub mod rip;
+pub mod rooms;
+pub mod save;
+pub mod scrolls;
+pub mod slime;
+pub mod splash {
+    pub mod load_sdl;
+    pub mod splash;
+} // mod splash
+pub mod sticks;
+pub mod strings;
+pub mod things;
+pub mod weapons;
+pub mod wizard;
 
 use std::process;
 
